302576c5a78890791e35f2c2bff9aa4ffb745e0f
alkimn <akimn@mit.edu>
2016-06-19 11:59:23 -0400

    UROP Project
    UROP

---
+++ codes/loadfiles.R
@@ -0,0 +1,35 @@
+###################################################################################
+# This script contains a single function to load the necessary files for analysis #
+###################################################################################
+
+Load.File <- function(directory, file){
+  # Function to load a single JSON table
+  # Takes two inputs: directory, file
+  # directory is the R-readable filepath to the folder that houses all the JSON log files
+  # file is character string of the name of the specific json table necessary e.g. "person_item" NOT "person_item.json"
+  
+  z <- list.files(directory)
+  # Obtain a character list of all the files within the given directory
+  
+  if (length(z) != 0){
+    # Make sure that the directory is not empty
+    
+    if (!paste(file, ".json", sep = "") %in% z ){
+      print(paste("WARNING: The file", file, "is missing", sep = " "))
+      return(NULL)
+      # If the file specified is not located within the given directory
+    }
+    
+    else{
+      name <- paste(directory, "\\", file, ".json", sep = "")
+      print(paste("Currently extracting data for the file: ", file, sep = ""))
+      return(fromJSON(sprintf("[%s]", paste(readLines(name), collapse=","))))
+    }
+    
+  }
+  else{
+    print("The given directory is empty or does not exist")
+    return(NULL)
+    
+  }
+}
